d0ae69f61750e57ec200562cc240d594088acac8
kbtem <kemalbaristemizoz@gmail.com>
2023-01-11 23:46:23 -0500

    Interview Tasks

---
+++ src/Week_Two/DivisibleBy.java
@@ -0,0 +1,24 @@
+package Week_Two;
+
+import java.sql.Array;
+
+public class DivisibleBy {
+    public static void main(String[] args) {
+
+       int[] arr = new int[100];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i+1;
+        }
+
+        for (int each : arr) {
+            if (each % 15 == 0&& each %3 ==0){
+
+            }
+        }
+
+
+
+    }
+
+}
---
+++ src/Week_Two/PrintConsecutiveNumbers.java
@@ -0,0 +1,4 @@
+package Week_Two;
+
+public class PrintConsecutiveNumbers {
+}
