916f765420256bfd6d150a8a64e966e49618dd8a
kbtem <kemalbaristemizoz@gmail.com>
2023-01-11 23:02:43 -0500

    Odd or even custom method

---
+++ src/Week_One/OddOrEven.java
@@ -0,0 +1,25 @@
+package Week_One;
+
+public class OddOrEven {
+
+    public static void oddOrEven(int number) {
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is even number.");
+        } else {
+            System.out.println(number + " is odd number.");
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        oddOrEven(300);
+
+
+    }
+
+
+
+
+}
